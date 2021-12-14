4ad03b5bd203b4ceecbeb735b0dc2aca598ca2d2
RickLyudmila <Lrick@yandex.ru>
2021-12-14 21:45:18 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/homework_map/HomeWorkMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.homework_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
